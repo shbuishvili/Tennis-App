83a51262728f8c2b35c67484f0f703c408c969ff
shbuishvili <sh.buishvili@gmail.com>
2026-08-02 22:16:51 +0400

    Add files via upload

--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -261,10 +261,10 @@ export default function BookingModal({
                     }
                   }}
                 >
-                  <option value="walk_1km">1კმ 10-15წთ</option>
-                  <option value="walk_2km">2კმ 25-30წთ</option>
-                  <option value="tour_4km">4.4კმ 45-60წთ</option>
-                  <option value="tour_7km">7კმ 1.5-2სთ</option>
+                  <option value="walk_1km">გასეირნება 1კმ 10-15წთ</option>
+                  <option value="walk_2km">გასეირნება 2კმ 25-30წთ</option>
+                  <option value="tour_4km">მარშრუტი 4.4კმ 45-60წთ</option>
+                  <option value="tour_7km">მარშრუტი 7კმ 1.5-2სთ</option>
                   <option value="pony_walk">პონით გასეირნება 10წთ</option>
                 </select>
               </div>
